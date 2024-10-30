205e0dcbff2fbd47bc46862004fc55a8d88898af
martin <martin.portgual228@gmail.com>
2024-10-30 16:07:10 +0100

    cutfreq aos limpio de clang-tidy

--- imgaos/cutfreq.hpp
+++ imgaos/cutfreq.hpp
@@ -17,19 +17,32 @@
 #include <cmath>
 #include <limits>
 #include <sstream>  // Incluimos <sstream> para std::istringstream
-#include <cmath> // Para std::sqrt
-#include <limits> // Para std::numeric_limits
-/*
-double distanciaEuclidiana(const Pixel8& first, const Pixel8& second);
-ImageAOS cutfreq(const ImageAOS& srcImage, const PPMMetadata& metadata, int nColores, const std::string& outputPath);
-void saveImage(const ImageAOS& image, const PPMMetadata& metadata, const std::string& outputPath);
-*/
+
+
 void cutfreq(ImageAOS& srcImage, const PPMMetadata& metadata, int nColores, const std::string& outputPath);
-//constexpr int combineRGB(int red, int green, int blue);
-//constexpr void extractRGB(int rgb, int& red, int& green, int& blue);
 void saveToPPM(const ImageAOS& srcImage, const PPMMetadata& metadata, const std::string& outputPath);
 template <typename T>
 constexpr Color<T> extractRGB(int rgb);
 template <typename T>
 constexpr int combineRGB(const Color<T>& color);
+
+//Pruebas
+void countColorFrequency(const ImageAOS& srcImage, std::unordered_map<int, int>& colorFrequency);
+void sortColorsByFrequency(std::unordered_map<int, int>& colorFrequency, std::vector<std::tuple<int, Color<int>>>& colorData);
+struct ColorSplit {
+  std::vector<std::tuple<int, Color<int>>> colorsToRemove;
+  std::vector<std::tuple<int, Color<int>>> colorsToKeep;
+};
+
+// Dividir colores en los que se mantienen y los que se eliminan
+ColorSplit splitColors(const std::vector<std::tuple<int, Color<int>>>& colorData, int nColores);
+struct ColorGroups {
+  std::vector<std::tuple<int, Color<int>>> colorsToRemove;
+  std::vector<std::tuple<int, Color<int>>> colorsToKeep;
+};
+
+Color<int> findClosestColor(const Color<int>& colorRem, const std::vector<std::tuple<int, Color<int>>>& colorsToKeep);
+void createReplacementMap(const ColorGroups& colorGroups,
+                          std::unordered_map<int, Color<int>>& replacementMap);
+void applyColorReplacement(ImageAOS& srcImage, const std::unordered_map<int, Color<int>>& replacementMap);
 #endif //CUTFREQ_HPP
