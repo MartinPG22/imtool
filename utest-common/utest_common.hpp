921d018ecb7c900dcecef364115f2071b477e8e8
martin <martin.portgual228@gmail.com>
2024-11-09 19:10:48 +0100

    cmake lists utest_common, utest_common.hpp y utest de binaryio

---
+++ utest-common/utest_common.hpp
@@ -0,0 +1,17 @@
+//
+// Created by mapor on 09/11/2024.
+//
+
+#ifndef UTEST_COMMON_HPP
+#define UTEST_COMMON_HPP
+
+#include "./common/binaryio.hpp"
+#include <gtest/gtest.h>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+constexpr int n_8 = 8;
+constexpr int n_13 = 13;
+#endif //UTEST_COMMON_HPP
